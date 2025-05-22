1623170425721bb7f396ddaf6959cac1cd012006
Enrique Puertas <puertasman@gmail.com>
2025-05-22 18:21:36 +0200

    comenzando playground

--- webempresarial/webempresarial/contact/views.py
+++ webempresarial/webempresarial/contact/views.py
@@ -1,6 +1,9 @@
+""" PAra el formulario """
+from ast import AsyncFunctionDef
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import ContactForm
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -15,6 +18,20 @@ def contact(request):
             email = request.POST.get('email', '')
             content = request.POST.get('content', '') 
             # Aquí enviaremos un correo
+            email = EmailMessage(
+                "La Caffettiera: Nuevo mensaje de contacto",
+                "De {}<{}>\nEscribió:\n\n{}".format(name, email, content),
+                "no-contestar@inbox.mailtrap.io",
+                ["puertasman@gmail.com"],
+                reply_to=[email]
+            )
+            try:
+                email.send()
+                # todo ha ido bien.
+                return redirect(reverse('contact')+"?ok")
+            except:
+                # Algo no ha ido bien, mandamos a fail
+                return redirect(reverse('contact')+"?fail")
             # Se supone que todo ha ido bien y redirigimos
             return redirect(reverse('contact')+"?ok")
 
--- webempresarial/webempresarial/social/admin.py
+++ webempresarial/webempresarial/social/admin.py
@@ -4,5 +4,11 @@ from .models import Link
 # Register your models here.
 class linkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
-
+    
+    def get_readonly_fields(self, request, obj = None):
+        if request.user.groups.filter(name='Personal').exists():
+            return ('key', 'name')
+        else:
+            return self.readonly_fields
+        
 admin.site.register(Link, linkAdmin)
